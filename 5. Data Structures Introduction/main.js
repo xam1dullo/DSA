0f3dd3a60c2bacf4405990a444c4a172a31f4c76
xam1dullo <mrx@swiftrelay.io>
2023-01-26 10:38:45 +0500

    5. Data Structures Introduction init

---
+++ 5. Data Structures Introduction/main.js
@@ -0,0 +1,10 @@
+/* 
+    1 . How to build One
+    2.  How to Use it
+
+
+
+    *  CPU - Central processing unit
+    *  RAM - Random Access memory
+    *  Storage - Storage is something you use to put your stuff in
+ */
